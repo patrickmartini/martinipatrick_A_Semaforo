67b74eabfaaaf0594acf9dc7bf4a5fb77b8a69d9
Patrick Martini <patrick.martini@barsanti.edu.it>
2019-02-09 09:15:56 +0100

    Versione Finale

--- ArduinoSemaforo/ArduinoSemaforo.ino
+++ ArduinoSemaforo/ArduinoSemaforo.ino
@@ -1,77 +1,108 @@
-int rosso = 1;
-int giallo = 2;
-int verde = 3;
-int rosso_ = 4;
-int giallo_ = 5;
-int verde_ = 6;
-int tempo = 100;
-
-void lampeggia_(int tempo) {
-  digitalWrite (verde_,LOW);
-  delay(tempo);
-  digitalWrite (verde_,HIGH);
-  delay(tempo);
-  digitalWrite (verde_,LOW);
-  delay(tempo);
-  digitalWrite (verde_,HIGH);
-  delay(tempo);
-  digitalWrite (verde_,LOW);
-  delay(tempo);
-  digitalWrite (verde_,HIGH);
-  delay(tempo);
-  digitalWrite (verde_,LOW);
-  delay(tempo);
-}
-
-void lampeggia(int tempo){
-  digitalWrite (verde,LOW);
-  delay(tempo);
-  digitalWrite (verde,HIGH);
-  delay(tempo);
-  digitalWrite (verde,LOW);
-  delay(tempo);
-  digitalWrite (verde,HIGH);
-  delay(tempo);
-  digitalWrite (verde,LOW);
-  delay(tempo);
-  digitalWrite (verde,HIGH);
-  delay(tempo);
-  digitalWrite (verde,LOW);
-  delay(tempo);
-}
+int rossoSemaforo1 = 2;
+int gialloSemaforo1 = 3;
+int verdeSemaforo1 = 4;
+int rossoSemaforo2 = 5;
+int gialloSemaforo2 = 6;
+int verdeSemaforo2 = 7;
+int tempo;
+int ripetizioni;
+int durataVerde;
+int durataGiallo;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(rosso, OUTPUT);
-  pinMode(giallo, OUTPUT);
-  pinMode(verde, OUTPUT);
-  pinMode(rosso_, OUTPUT);
-  pinMode(giallo_, OUTPUT);
-  pinMode(verde_, OUTPUT);
+  
+  Serial.begin(9600);
+  tempoVerde();
+  tempoGiallo();
+  tempoLampeggio();
+  conteggioLampeggi();
+  pinMode(rossoSemaforo1, OUTPUT);
+  pinMode(gialloSemaforo1, OUTPUT);
+  pinMode(verdeSemaforo1, OUTPUT);
+  pinMode(rossoSemaforo2, OUTPUT);
+  pinMode(gialloSemaforo2, OUTPUT);
+  pinMode(verdeSemaforo2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite (rosso_, LOW);
-  digitalWrite (giallo_, LOW);
-  digitalWrite (rosso, HIGH);
-  digitalWrite (verde_, HIGH);
-  delay(4600);
 
-  digitalWrite (giallo, HIGH);
-  digitalWrite (giallo_, HIGH);
-  lampeggia_(tempo);
+  // accendo il rosso ed il verde dei due semafori,
+  //mentre tengo spento il giallo del primo semaforo, 
+  //ed il rosso ed il verde del secondo
+  digitalWrite (rossoSemaforo2, LOW);
+  digitalWrite (gialloSemaforo2, LOW);
+  digitalWrite (gialloSemaforo1, LOW);
+  digitalWrite (rossoSemaforo1, HIGH);
+  digitalWrite (verdeSemaforo2, HIGH);
+  delay(durataVerde);
 
-  digitalWrite (rosso, LOW);
-  digitalWrite (giallo, LOW);
-  digitalWrite (verde, HIGH);
-  digitalWrite (rosso_, HIGH);
-  delay(4600);
-  
-  lampeggia(tempo);
-  digitalWrite (giallo, HIGH);
-  delay(5000);
+  //accendo il giallo
+  digitalWrite (gialloSemaforo1, HIGH);
+
+  // faccio lampeggiare (tramite il metodo)
+  //il verde del secondo semaforo
+  lampeggio(verdeSemaforo2, tempo, ripetizioni);
+
+  //accendo il giallo del secondo semaforo
+  //usando il tempo che abbiamo indicato all'avvio del programma
+  digitalWrite (gialloSemaforo2, HIGH);
+  delay(durataGiallo);
+
+  //accendo il verde del primo semaforo ed il rosso del secondo
+  //mentre spengo il rosso ed il giallo del primo semaforo,
+  //ed il giallo del secondo semaforo. IL verde usa il tempo
+  //che abbiamo indicato all'avvio del programma
+  digitalWrite (rossoSemaforo1, LOW);
+  digitalWrite (gialloSemaforo1, LOW);
+  digitalWrite (verdeSemaforo1, HIGH);
+  digitalWrite (gialloSemaforo2, LOW);
+  digitalWrite (rossoSemaforo2, HIGH);
+  delay(durataVerde);
+
+  //
+  digitalWrite (gialloSemaforo2, HIGH);
+  lampeggio(verdeSemaforo1, tempo, ripetizioni);
+  digitalWrite (gialloSemaforo1, HIGH);
+  delay(durataGiallo);
   
 }
 
-//prova gitkraken
+// durata di UN lampeggio
+void tempoLampeggio(){
+  Serial.println ("quanto vuoi far durare un lampeggio in millisecondi?");
+  while(Serial.available() == 0) {};
+  tempo = Serial.readString().toInt();
+}
+
+//numero dei lampeggi
+void conteggioLampeggi(){
+  Serial.println ("quante volte vuoi far lampeggiare il verde?");
+  while(Serial.available() == 0) {};
+  ripetizioni = Serial.readString().toInt();
+}
+
+//durata del verde
+void tempoVerde(){
+  Serial.println ("quanto vuoi far durare il verde in millisecondi?");
+  while(Serial.available() == 0) {};
+  durataVerde = Serial.readString().toInt();
+}
+
+//durata del giallo
+void tempoGiallo(){
+  Serial.println ("quanto vuoi far durare il giallo in millisecondi?");
+  while(Serial.available() == 0) {};
+  durataGiallo = Serial.readString().toInt();
+}
+
+//fa fare i lampeggi
+void lampeggio(int led, int tempo, int ripetizioni) {
+  for (int i = 0;i <= ripetizioni;i++)
+  {
+  digitalWrite (led,LOW);
+  delay(tempo);
+  digitalWrite (led,HIGH);
+  delay(tempo);
+  digitalWrite (led,LOW);
+  }
+}
